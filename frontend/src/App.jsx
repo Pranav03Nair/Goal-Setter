dbf18a654a3fd5ba1856870ac464498882a26855
Pranav03Nair <pp6275@srmist.edu.in>
2025-09-02 12:39:31 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <div className="flex min-h-screen flex-col gap-4 bg-background text-text">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
